76157e53dd2cb513f5cb087642c0b337add7d299
axi92 <phips_92@yahoo.de>
2015-05-04 19:47:32 +0200

    Initial commit (Arma3 + TS3)
    Initial commit for status page for arma 3 and ts3

--- Example.php
+++ Example.php
@@ -1,13 +1,20 @@
 <?php
 	require __DIR__ . '/SourceQuery/SourceQuery.class.php';
-	
+	require_once(__DIR__ . '/libraries/TeamSpeak3/TeamSpeak3.php');
 	// For the sake of this example
-	Header( 'Content-Type: text/plain' );
+	//Header( 'Content-Type: text/plain' );
 	Header( 'X-Content-Type-Options: nosniff' );
 	
 	// Edit this ->
-	define( 'SQ_SERVER_ADDR', 'localhost' );
-	define( 'SQ_SERVER_PORT', 27015 );
+	define( 'SQ_SERVER_ADDR', '37.187.170.178' ); //zero one
+	//define( 'SQ_SERVER_ADDR', '31.186.251.213' );
+	/*
+	GEHT:
+	81.88.208.198	75 Spieler
+	85.159.42.196	78
+	31.186.251.213	74
+	*/
+	define( 'SQ_SERVER_PORT', 2303 );
 	define( 'SQ_TIMEOUT',     1 );
 	define( 'SQ_ENGINE',      SourceQuery :: SOURCE );
 	// Edit this <-
@@ -17,10 +24,12 @@
 	try
 	{
 		$Query->Connect( SQ_SERVER_ADDR, SQ_SERVER_PORT, SQ_TIMEOUT, SQ_ENGINE );
-		
+		echo '<pre>';
 		print_r( $Query->GetInfo( ) );
-		print_r( $Query->GetPlayers( ) );
-		print_r( $Query->GetRules( ) );
+		echo $Query->GetInfo()['Players'];
+		//print_r( $Query->GetPlayers( ) );
+		//print_r( $Query->GetRules( ) );
+		echo '</pre>';
 	}
 	catch( Exception $e )
 	{
@@ -28,3 +37,12 @@
 	}
 	
 	$Query->Disconnect( );
+
+	//$ts_ip = "ts.zero-one.cc"; // Change to your server's IP external or domain name
+	//$ts_ip = "udp://ts.zero-one.cc"; // Change to your server's IP external or domain name
+	$ts_ip = "ts.zero-one.cc"; // Change to your server's IP external or domain name
+	$ts_port = "10011"; // Make sure this port is open on the router or firewall
+
+	$online0 = @fsockopen($ts_ip, $ts_port, $errno, $errstr, 1);
+	if($online0 >= 1) echo 'ONLINE';
+	else echo 'OFFLINE';
